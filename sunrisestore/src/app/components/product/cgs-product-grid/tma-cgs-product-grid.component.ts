51cfb0b53ee7638de2c36dc0ed210aa18a819151
Brigitta Balazs <brigitta.balazs@ibm.com>
2020-03-23 11:40:44 +0200

    image added to product view in guided selling

--- sunrisestore/src/app/components/product/cgs-product-grid/tma-cgs-product-grid.component.ts
+++ sunrisestore/src/app/components/product/cgs-product-grid/tma-cgs-product-grid.component.ts
@@ -1,22 +1,36 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@angular/core";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Media, MediaService} from "@spartacus/storefront";
+import {TmaProduct} from "../../../model/tma-product.model";
+import {ProductService} from "@spartacus/core";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'cx-cgs-product-grid-item',
   templateUrl: './tma-cgs-product-grid.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TmaCgsProductGridComponent {
+export class TmaCgsProductGridComponent implements OnInit {
   @Input()
   product: any;
 
   @Output()
   changeSpoInBpo = new EventEmitter<any>();
 
-  constructor() {
+  productDb$: Observable<TmaProduct>;
+
+  constructor(
+    protected productService: ProductService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.productDb$ = this.productService.get(this.product.code);
 
   }
 
   select(productCode: string) {
     this.changeSpoInBpo.emit(productCode);
   }
+
+
 }
